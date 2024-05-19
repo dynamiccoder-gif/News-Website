4434e92f2029b4bd0b3bd71fe66ab5c6bbe8c00c
ROHIT KUMAR <74416087+dynamiccoder-gif@users.noreply.github.com>
2024-05-19 11:04:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,27 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import LoadingBar from 'react-top-loading-bar'
 
 import './App.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Navbar/> ,
-  children:[
-    
-  {path:"/",
-   element:<Home />},
-   {path:"Business",
-   element:<Home  key="Business" country="in" category="Business"/>},
-   {path:"Entertainment",
-   element:<Home  key="Entertainment" country="in" category="Entertainment"/>},
-   {path:"Technology",
-   element:<Home  key="Technology" country="in" category="Technology"/>},
-   {path:"Sports",
-   element:<Home   key="Sports" country="in" category="Sports"/>},
-   {path:"Science",
-   element:<Home  key="Scaince" country="in" category="Science"/>},
-   {path:"Health",
-   element:<Home  key="Health" country="in" category="Health"/>}
-  ]
-}
-   
-]);
 
 export default class App extends Component {
- 
   render() {
     return (
       <div className='app1'>
-   
-      <RouterProvider router={router} />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Business' element={<Home country='in' category='Business' />} />
+            <Route path='/Entertainment' element={<Home country='in' category='Entertainment' />} />
+            <Route path='/Technology' element={<Home country='in' category='Technology' />} />
+            <Route path='/Sports' element={<Home country='in' category='Sports' />} />
+            <Route path='/Science' element={<Home country='in' category='Science' />} />
+            <Route path='/Health' element={<Home country='in' category='Health' />} />
+          </Routes>
+        </Router>
       </div>
     )
   }
